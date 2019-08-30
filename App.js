9e83ba5faa0b3762e128091d06777f87560cb6e3
Malov Victor <malvikus@gmail.com>
2019-08-30 14:44:58 +0300

    Add example with width and height

--- App.js
+++ App.js
@@ -9,35 +9,31 @@ const StyledView = styled.View`
 `;
 
 const StyledBox = styled.View`
-  width: 150;
-  height: 150;
   background-color: tomato;
 `;
 
 const App = () => {
-  const animation = new Animated.Value(1);
+  const animation = new Animated.Value(150);
   const [animatedStyles, setAnimatedStyles] = useState({
-    transform: [
-      {
-        scale: animation,
-      },
-    ],
+    width: animation,
+    height: animation,
   });
 
   const startAnimation = () => {
     Animated.timing(animation, {
-      toValue: 2,
+      toValue: 300,
       duration: 1500,
     }).start(() => {});
   };
   return (
     <StyledView>
       <TouchableWithoutFeedback onPress={startAnimation}>
-        <Animated.View style={animatedStyles}>
-          <StyledBox>
-            <Text>This side forward</Text>
-          </StyledBox>
-        </Animated.View>
+        <StyledBox as={Animated.View} style={animatedStyles}>
+          <Text>
+            this is a really long text. This is a really long text. This is a
+            really long text.
+          </Text>
+        </StyledBox>
       </TouchableWithoutFeedback>
     </StyledView>
   );
